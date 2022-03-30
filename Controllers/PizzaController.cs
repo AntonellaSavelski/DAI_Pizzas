000386b02a07160fa342c28e7d8e882c2846f5ee
Admision <admision@ort>
2022-03-30 10:42:22 -0300

    clase 30-3

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Logging;
 using Pizzas.API.Models;
 using System.Data.SqlClient;
 using Dapper;
+using Pizzas.API.Utils;
+using Pizzas.API.Services;
+using Pizzas.API.Helpers;
 
 namespace Pizzas.API.Controllers
 {
@@ -14,43 +17,29 @@ namespace Pizzas.API.Controllers
     [Route("api/[controller]")]
     public class PizzaController : ControllerBase
     {  
-        public static List<Pizza> _ListadoPizzas = new List<Pizza>();
-
         [HttpGet]
         public IActionResult GetAll()
         {
-            using (SqlConnection db = new SqlConnection(basededatos._connectionString)){
-                string sql = "SELECT * from Pizzas";
-                _ListadoPizzas = db.Query<Pizza>(sql).ToList();
-            }
-            return Ok (_ListadoPizzas);
+            return Ok (PizzasServices.GetAll(_ListadoPizzas));
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id){
-            Pizza unaPizza;
-            using (SqlConnection db = new SqlConnection(basededatos._connectionString)){
-                string sql = "SELECT * from Pizzas WHERE id = @pId";
-                unaPizza = db.QueryFirstOrDefault<Pizza>(sql, new {pId = id});
-            }
-            return Ok(unaPizza);
+            
+            return Ok(PizzasServices.GetById(unaPizza));
         }
 
         [HttpPost]
         public IActionResult Create(Pizza pizza){
-            int nuevaPizza;
-            string sql = "INSERT into Pizzas (Nombre, LibreGluten, Importe, Descripcion) VALUES (@pNombre, @pLibreGluten, @pImporte, @pDescripcion)";
-            using (SqlConnection db = new SqlConnection(basededatos._connectionString)){
-               nuevaPizza = db.Execute(sql, new {pNombre=pizza.Nombre, pLibreGluten=pizza.LibreGluten, pImporte=pizza.Importe, pDescripcion=pizza.Descripcion});
-            } 
-            return Ok(pizza);
+        
+            return Ok(PizzasServices.Create(pizza));
         }
 
         [HttpPut("{id}")]
         public IActionResult Update(int id, Pizza pizza){
             int nuevaPizza;
             string sql = "UPDATE Pizzas SET Nombre = @pNombre, LibreGluten = @pLibreGluten, Importe = @pImporte, Descripcion = @pDescripcion WHERE id = @pId";
-            using (SqlConnection db = new SqlConnection(basededatos._connectionString)){
+            using (SqlConnection db = basededatos.GetConnection()){
                nuevaPizza = db.Execute(sql, new {pId= id, pNombre=pizza.Nombre, pLibreGluten=pizza.LibreGluten, pImporte=pizza.Importe, pDescripcion=pizza.Descripcion});
             } 
             return Ok(pizza);
@@ -60,7 +49,7 @@ namespace Pizzas.API.Controllers
         public IActionResult DeleteById(int id){
             Pizza pizzaABorrar;
             string sql = "DELETE from Pizzas WHERE id = @pId";
-            using (SqlConnection db = new SqlConnection(basededatos._connectionString)){
+            using (SqlConnection db = basededatos.GetConnection()){
                pizzaABorrar = db.QueryFirstOrDefault<Pizza>(sql, new {pId = id});
             } 
             return Ok(pizzaABorrar);
--- Controllers/bd.cs
+++
@@ -1,9 +0,0 @@
-using System.Data.SqlClient;
-using Dapper;
-
-namespace Pizzas.API.Controllers
-{
-    public class basededatos {
-        public static string _connectionString = @"server=A-CEO-04; DataBase=DAI-Pizzas; Trusted_Connection=True;";
-    }
-}
